fefe475be22c2fb1ab7f44e2e6b2092aed27c1ec
Andrew <andrewyin1994@gmail.com>
2019-06-10 08:15:58 -0700

    item factory and validator complete

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,22 @@
+'use strict';
+/* global cuid */
 
-const Item = (function(){
-    let obj = {};
-    return obj;
+
+const Item = (function() {
+  function validateName(name) {
+    if (!name) throw new TypeError('Name does not exist.');
+  }
+
+  function create(name) {
+    return {
+      name,
+      id: cuid(),
+      checked: false
+    }
+  }
+
+  return {
+    create,
+    validateName
+  };
 }());
--- scripts/store.js
+++ scripts/store.js
@@ -3,10 +3,10 @@
 
 const store = (function() {
   let items = [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
+    // { id: cuid(), name: 'apples', checked: false },
+    // { id: cuid(), name: 'oranges', checked: false },
+    // { id: cuid(), name: 'milk', checked: true },
+    // { id: cuid(), name: 'bread', checked: false }
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
